617845adc0b23d7f54670b2c7d9e311b5396b4b9
Damian Esteban <damian.esteban@gmail.com>
2022-02-25 22:27:08 -0500

    ok

--- apps/user-service/src/main.ts
+++ apps/user-service/src/main.ts
@@ -1,6 +1,6 @@
 const start = async () => {
   setTimeout(async () => {
-    console.log('Hello aaaaaWorld!!!!sssss!!ddd');
+    console.log('Hello aaaassssaWorld!!!!sssss!!ddd');
     await Promise.resolve('foo');
   }, 5000);
 };
