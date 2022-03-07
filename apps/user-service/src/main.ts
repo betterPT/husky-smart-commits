48ecd88bd25ca02215d6782816a00bf47998f4ed
Damian Esteban <damian.esteban@gmail.com>
2022-03-06 21:13:40 -0500

    Add husky

--- apps/user-service/src/main.ts
+++ apps/user-service/src/main.ts
@@ -1,6 +1,6 @@
 const start = async () => {
   setTimeout(async () => {
-    console.log('Hello aaaassssaWorld!!!!sssss!!ddd');
+    console.log('Hello aaaassssaWorld!11111!!!sssss!!ddd');
     await Promise.resolve('foo');
   }, 5000);
 };
