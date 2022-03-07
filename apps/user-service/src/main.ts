bff4dd5068de7473af8937081235d66bb8875a90
Damian Esteban <damian.esteban@gmail.com>
2022-03-06 21:20:42 -0500

    [BC-1024] Another test

--- apps/user-service/src/main.ts
+++ apps/user-service/src/main.ts
@@ -1,6 +1,6 @@
 const start = async () => {
   setTimeout(async () => {
-    console.log('Hello aaaassssaWorld!1QQQQQQQ1111!!!sssss!!ddd');
+    console.log('Hello aaaassssaWorld!1QjjjjjQQQQQQ1111!!!sssss!!ddd');
     await Promise.resolve('foo');
   }, 5000);
 };
